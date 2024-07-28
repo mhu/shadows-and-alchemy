f13bd7d090c62f6bcf35f65ac01357ffdfc9f31e
mhu <marcel.huismann@protonmail.ch>
2024-07-28 13:25:16 -0400

    feat: implement mute functionality

--- scripts/music_singleton.gd
+++ scripts/music_singleton.gd
@@ -2,6 +2,7 @@ extends Node
 
 var music := AudioStreamPlayer.new()
 var sound := AudioStreamPlayer.new()
+var is_muted: bool = false
 
 
 func _ready() -> void:
@@ -14,6 +15,18 @@ func _ready() -> void:
     sound.stream = load("res://assets/music/shadows-and-alchemy-hit.mp3")
     sound.process_mode = Node.PROCESS_MODE_ALWAYS
     add_child(sound)
+
+
+func _process(_delta: float) -> void:
+    if Input.is_action_just_pressed("mute"):
+        var index := AudioServer.get_bus_index("Master")
+
+        if is_muted:
+            is_muted = false
+            AudioServer.set_bus_mute(index, false)
+        else:
+            is_muted = true
+            AudioServer.set_bus_mute(index, true)
 
 
 func play_enemy_hit() -> void:
