16c3b5a4dc64795a152881359c4acc2fa8913ff2
mhu <marcel.huismann@protonmail.ch>
2024-07-29 21:53:43 -0400

    feat: randomize exit portal location

--- scripts/main.gd
+++ scripts/main.gd
@@ -9,6 +9,17 @@ func _ready() -> void:
     get_tree().set_pause(false)
     Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 
+    randomize_exit_location()
+
+
+func randomize_exit_location() -> void:
+    if not has_node("ExitLocations"):
+        return
+
+    var exit_node: AnimatedSprite2D = $Exit
+    var exit_location_node: Node2D = $ExitLocations.get_children().pick_random()
+    exit_node.global_position = exit_location_node.global_position
+
 
 func _process(delta: float) -> void:
     elapsed_time += delta
